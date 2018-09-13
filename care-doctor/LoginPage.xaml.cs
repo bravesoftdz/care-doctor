2772218f6888bcb66da07ab707a941dba10a43ac
kabilan <kabilan@careme.io>
2018-09-13 19:56:47 +0530

    [Raw] Login page layout

---
+++ care-doctor/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace care_doctor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignup(object sender, EventArgs e){
+            LoginStack.IsVisible = false;
+            SignupStack.IsVisible = true;
+        }
+
+        private void BtnRequestOtp(object sender, EventArgs e){
+            txtEmailOtp.IsVisible = true;
+            btnLogin.IsVisible = true;
+            btnRequestOtp.IsVisible = false;
+        }
+
+        private void BtnSignupComplete(object sender, EventArgs e){
+            LoginStack.IsVisible = true;
+            SignupStack.IsVisible = false;
+        }
+    }
+}
